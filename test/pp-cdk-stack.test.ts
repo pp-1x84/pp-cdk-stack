e4377a9efe22ffde81bf79324ad26d52a88e3b8f
pp-1x84 <j-alan@protonmail.com>
2024-05-04 09:12:58 -0400

    Initial commit

---
+++ test/pp-cdk-stack.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as PpCdkStack from '../lib/pp-cdk-stack-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/pp-cdk-stack-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new PpCdkStack.PpCdkStackStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
